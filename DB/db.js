f004a4730958f6eeee8631d85f7071077506fd04
dibyajyoti prakash <dibyajyoti.prakash@estpl.in>
2024-08-31 18:53:14 +0530

    changes db

--- DB/db.js
+++ DB/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mytest", {
+    await mongoose.connect("mongodb+srv://dibyazynox:dibyazynox%4079768@zovio-api.znyfsqi.mongodb.net/", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
